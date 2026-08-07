a5105da54e17fa3df9c968ccaf46f5417f7a7aa4
Renish Vekariya <renishrac@gmail.com>
2026-08-07 21:23:10 +0530

    coutntry dto dded

---
+++ DemoTestCaseAutomation.Application/DTOs/CountryDto.cs
@@ -0,0 +1,8 @@
+namespace DemoTestCaseAutomation.Application.DTOs;
+
+public class CountryDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+}
---
+++ DemoTestCaseAutomation.Domain/Entities/Country.cs
@@ -0,0 +1,8 @@
+namespace DemoTestCaseAutomation.Domain.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+}
